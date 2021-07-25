66c9acbb6be52f13eafd55a4edbb227c000c7f50
mvuente <alexeymavrin@yandex.ru>
2021-07-25 17:11:02 +0300

    cleaning

--- src/main/kotlin/ru/phonebook/phonebook/services/NameService.kt
+++
@@ -1,8 +0,0 @@
-package ru.phonebook.phonebook.services
-
-import ru.phonebook.phonebook.entities.NameEntity
-
-interface NameService {
-    fun findAll(): List<NameEntity>
-    fun findByName(name: String): NameEntity?
-}
